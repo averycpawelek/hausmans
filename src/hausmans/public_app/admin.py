6dbc3093eb4930802c82ef5833e8c621018df799
Avery Pawelek <averycpawelek@gmail.com>
2020-12-28 12:26:44 -0600

    Added models for projects

---
+++ src/hausmans/public_app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from public_app.models import PortfolioProject, PortfolioImage
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('project', 'created_at')
+
+
+admin.site.register(PortfolioProject, ProjectAdmin)
+admin.site.register(PortfolioImage, ImageAdmin)
---
+++ src/hausmans/public_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class PortfolioProject(models.Model):
+    name = models.CharField(max_length=1000)
+    thumbnail_image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class PortfolioImage(models.Model):
+    project = models.ForeignKey(PortfolioProject, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='portfolio_images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
